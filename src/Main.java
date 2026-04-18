b0888fa5f06a267d0f2c5b5dff44198f4567eba1
julia-olenin <julia86ru@gmail.com>
2026-04-18 12:48:41 +0300

    add sum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 void main() {
     IO.println("Hello and welcome!");
+    int sum = 0;
     for (int i = 1; i <= 5; i++) {
+        sum += i;
         IO.println("i = " + i);
     }
+    IO.println("Сумма" + sum);
 }
